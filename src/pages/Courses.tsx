3838a3561cd1935bd2bbd611417bbe050e981227
Antoine B <aberthillot@myges.fr>
2025-10-06 12:07:42 +0200

    Added courses & cyclits pages

---
+++ src/pages/Courses.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Courses: React.FC = () => {
+    return <h2>Liste des courses</h2>; //WIP
+};
+
+export default Courses;
---
+++ src/pages/Cyclists.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Cyclists: React.FC = () => {
+    return <h2>Liste des cyclistes</h2>; //WIP
+};
+
+export default Cyclists;
